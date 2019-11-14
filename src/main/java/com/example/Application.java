315510104d75fcb045ed3e8107dd87fd20d3ebeb
gaoshja <gaoshja@yonyou.com>
2019-11-14 10:10:51 +0800

    iuap培训

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -3,10 +3,13 @@ package com.example;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @MapperScan("com.example.mapper")
-public class Application {
+@ServletComponentScan
+public class Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
