2e761a0c8e9f244a64171f69f7bf80f81c2386b4
gaoshja <gaoshja@yonyou.com>
2019-11-19 14:08:37 +0800

    iuap培训+logback

--- src/main/java/com/example/comtroller/StuController.java
+++ src/main/java/com/example/comtroller/StuController.java
@@ -6,6 +6,8 @@ import com.github.pagehelper.PageInfo;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,7 +17,7 @@ import java.util.List;
 @RestController
 @Slf4j
 public class StuController {
-
+    private final static Logger logger = LoggerFactory.getLogger(StuController.class);
 
     @Autowired
     private StuService stuService;
@@ -23,6 +25,9 @@ public class StuController {
     @ApiOperation(value = "查询用户" ,  notes="查询全部用户")
     @GetMapping("/findAll")
     public List<Stu> findAll() {
+        logger.debug("记录debug日志");
+        logger.info("访问了index方法");
+        logger.error("记录了error错误日志");
 
         return stuService.findAll();
     }
